7efbba6c09e47bee531524c2523a5beeba46649b
rgantzos <info@gantzos.com>
2022-12-10 22:30:58 -0800

    don't send them again

--- index.js
+++ index.js
@@ -684,10 +684,10 @@ These rules aren’t meant to be tiring and mods reserve the right to punish you
       .setStyle("Link")
       .setURL("https://docs.scratchtools.app")
   );
-  await channel.send({embeds:[mainEmbed]})
-  await channel.send({embeds:[rulesEmbed]})
-  await channel.send({embeds:[getRoles]})
-  await channel.send({components:[components]})
+  //await channel.send({embeds:[mainEmbed]})
+  //await channel.send({embeds:[rulesEmbed]})
+  //await channel.send({embeds:[getRoles]})
+  //await channel.send({components:[components]})
 });
 
 client.on("guildMemberRemove", async function (member) {
