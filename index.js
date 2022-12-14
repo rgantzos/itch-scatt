e615575a2a2b09a2657dcfb152dd354cfe864c6f
rgantzos <info@gantzos.com>
2022-12-13 17:43:54 -0800

    seriously

--- index.js
+++ index.js
@@ -740,7 +740,7 @@ async function getLeaderboard() {
       }
     });
   });
-  console.log(await dbClient.db("Scatt").collection("userdata").find({}).sort( { "xp": 1 } )).toArray()
+  console.log(await dbClient.db("Scatt").collection("userdata").find({}).sort( { "xp": 1 } ).toArray())
   return topAll.reverse();
 }
 
